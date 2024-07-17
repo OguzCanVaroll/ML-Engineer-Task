6eb50eb9624eb7af795e885c6e57c03dbd62ab61
Oguz Can Varol <oguzcnvaroll@gmail.com>
2024-07-17 22:03:45 +0300

    last

--- scraper.py
+++ scraper.py
@@ -12,25 +12,36 @@ def get_data():
         soup = BeautifulSoup(response.text, 'html.parser')
         products = []
         for product in soup.select('.product'):
+            
             name = product.select_one('.woocommerce-loop-product__title').text
+            
             price = product.select_one('.price').text
+            
             product_link = product.select_one('a')['href']
+            
             product_response = requests.get(product_link)
+            
+            
             if product_response.status_code == 200:
                 product_soup = BeautifulSoup(product_response.text, 'html.parser')
                 description = product_soup.select_one('.woocommerce-product-details__short-description').text if product_soup.select_one('.woocommerce-product-details__short-description') else "No description available"
                 description = description.replace('\n', ' ').strip()
+            
             else:
                 description = "No description available"
             products.append({'name': name, 'price': price, 'description': description})
         return products
+    
     else:
         print("Failed to retrieve data")
         return None
 
 def main():
-    time.sleep(30)  # Kafka'nın tam olarak başlaması için bekleme süresi ekleyelim
+    
+    time.sleep(30) 
+    
     producer = KafkaProducer(
+        
         bootstrap_servers='kafka:9092',
         value_serializer=lambda v: json.dumps(v).encode('utf-8')
     )
